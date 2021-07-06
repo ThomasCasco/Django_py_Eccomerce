6200f7f69454fd4abf33c728a606082d3e161cc7
Thomas <thomas.s.casco@gmail.com>
2021-07-06 19:57:57 -0300

    changes login

--- eccomerce/ecomsite/ecomsite/urls.py
+++ eccomerce/ecomsite/ecomsite/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
 from shop import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name='index'),
     path('<int:id>/',views.detail,name='detail'),
     path('checkout/',views.checkout,name='checkout'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
